3b95db8b90691521449d8125671f6dd4dfcf72b3
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2025-06-20 17:07:50 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Defina a data e hora do evento para o qual você está fazendo a contagem regressiva
+const countDownDate = new Date("Jul 16, 2025 10:30:00").getTime();
+
+// Atualiza a contagem regressiva a cada 1 segundo
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    // Calcula o tempo restante
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Exibe o resultado no elemento com o id "countdown"
+    document.getElementById("countdown").innerHTML = "Viagem: "+days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // Se a contagem regressiva terminar, exibe uma mensagem
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "Bora para Curitiba!";
+    }
+}, 1000);
